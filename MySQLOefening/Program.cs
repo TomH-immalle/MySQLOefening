df65f29949db0fd39e7c3c5487af49373dfa5283
Tom Hollestelle <tom.hollestelle@immalle.eu>
2017-09-28 10:05:35 +0200

    oefening

--- MySQLOefening/Program.cs
+++ MySQLOefening/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 
 namespace MySQLOefening
 {
     class Program
     {
+        static string version = "0.1";
+        public static void Query()
+        {
+            MySqlConnection comm = new MySqlConnection();
+            comm.ConnectionString = "Server = 192.168.56.101;Port = 3306; Database = world;Uid = root;Pwd = root; ";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = comm;
+
+            comm.Open();
+
+            cmd.CommandText = "SELECT count(*) FROM Country";
+
+            Console.WriteLine("{0}", cmd.ExecuteScalar());
+        }
+        public static void Query2()
+        {
+            MySqlConnection comm = new MySqlConnection();
+            comm.ConnectionString = "Server = 192.168.56.101;Port = 3306; Database = world;Uid = root;Pwd = root; ";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = comm;
+
+            comm.Open();
+
+            cmd.CommandText = "SELECT Name FROM Country";
+
+            Console.WriteLine("{0}", cmd.ExecuteReader());
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "-v":
+                        Console.WriteLine("Versie {0}", version);
+                        break;
+                    case "-allcountries":
+                        Query();
+                        break;
+                    case "-countries":
+                        Query2();
+                        break;
+                    default:
+                        Console.WriteLine("Onbekend argument");
+                        break;
+                }
+                //Console.WriteLine(args[0]);
+            }
         }
     }
 }
